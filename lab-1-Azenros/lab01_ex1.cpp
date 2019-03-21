a0987e685d4300376cfd72a884a2689f6deb80fb
Steven Tran <stran050@ucr.edu>
2019-03-21 16:55:15 -0700

    upload test (lab 1)

---
+++ lab-1-Azenros/lab01_ex1.cpp
@@ -0,0 +1,19 @@
+//=================================================
+// Name: Tran, Steven
+// Email: stran050@ucr.edu
+// 
+// Lab: 1
+// Lab section: 25
+// TA: Daniel Handojo
+// 
+//=================================================
+// Write C++ "Hello World" program.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  return 0;
+}
